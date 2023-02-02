4a568cc89daf4b2edfc92c88b38fbc1ddac39ced
Nick Bellos <49724459+nickbel7@users.noreply.github.com>
2023-02-02 03:13:48 +0200

    Create download-data.sh

---
+++ download-data.sh
@@ -0,0 +1,9 @@
+#download trip-data from January to June
+wget https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-01.parquet
+wget https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-02.parquet
+wget https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-03.parquet
+wget https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-04.parquet
+wget https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-05.parquet
+wget https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-06.parquet
+#download taxi_zone lookup file, which connects Location_ID with suburbs and zones
+wget https://d37ci6vzurychx.cloudfront.net/misc/taxi+_zone_lookup.csv
