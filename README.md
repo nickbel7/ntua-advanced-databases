d69250cc6f8535c1503973330ac2df0cc237b766
ElinaSyr <elinasyrr@gmail.com>
2023-02-04 21:33:08 +0200

    Added comments to download-data.sh file

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # ntua-advanced-databases
 Term project for the course 'Advanced Databases' during 9th semester at NTUA
-Authors: 
-@nickbel 
-@elinasyr
--- download-data.sh
+++ download-data.sh
@@ -1,9 +1,12 @@
-#download trip-data from January to June
+# Download taxi-trip data from here: https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page
+# Specifically we download all the files from January to June Year 2022
 wget https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-01.parquet
 wget https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-02.parquet
 wget https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-03.parquet
 wget https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-04.parquet
 wget https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-05.parquet
 wget https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-06.parquet
-#download taxi_zone lookup file, which connects Location_ID with suburbs and zones
+
+# Download here: https://d37ci6vzurychx.cloudfront.net/misc/taxi+_zone_lookup.csv
+# This csv is the linkage LocationID and Zone/Borough Location 
 wget https://d37ci6vzurychx.cloudfront.net/misc/taxi+_zone_lookup.csv
